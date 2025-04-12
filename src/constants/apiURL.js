2e57bbe2d1613968aa79d67099873e76ffcf7e15
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2025-04-12 09:49:56 +0700

    Update API base URL to production environment

--- src/constants/apiURL.js
+++ src/constants/apiURL.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:4000/api",
+//   withCredentials: true, // ⚠️ Cần thiết để nhận cookie từ server
+// });
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://moyarperfume.netlify.app",
   withCredentials: true, // ⚠️ Cần thiết để nhận cookie từ server
 });
 
