7772e25904fe0d0c13f6765aa52fbb12d48edef4
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2025-04-18 15:33:23 +0700

    Update API base URL to point to the production server and comment out local development configurations

--- src/constants/apiURL.js
+++ src/constants/apiURL.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:4000/api",
+//   withCredentials: true, // ⚠️ Cần thiết để nhận cookie từ server
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "http://website-phi-five-69.vercel.app/api",
   withCredentials: true, // ⚠️ Cần thiết để nhận cookie từ server
 });
+
 // const api = axios.create({
 //   baseURL: "https://moyarperfume.netlify.app",
 //   withCredentials: true, // ⚠️ Cần thiết để nhận cookie từ server
